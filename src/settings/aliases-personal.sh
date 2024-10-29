0ca68f9356dd668d20d960d70b561e3e7934987a
Bruno Borges Russian <bruno.russian.i@gmail.com>
2024-10-28 20:51:36 -0400

    Fix typo

--- src/settings/aliases-personal.sh
+++ src/settings/aliases-personal.sh
@@ -2,6 +2,6 @@ alias use-dotfiles="cd ${HOME}/dotfiles"
 alias zsh-config="code ~/.zshrc"
 alias zsh-reset="source ~/.zshrc"
 alias clean-docker="docker stop \$(docker ps -aq) && docker rm \$(docker ps -aq)"
-alias itsmine="sudo chwn -R $USER:$USER ."
+alias itsmine="sudo chown -R $USER:$USER ."
 
 alias lsg="ls -al | grep ${1}"
